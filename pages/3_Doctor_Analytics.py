69fe448e788832e40e5f5d1afd5322d16a272e50
shraddhaballambettu-student <shraddhaballambettu@gmail.com>
2026-06-04 12:18:51 +0530

    Update 3_Doctor_Analytics.py

--- pages/3_Doctor_Analytics.py
+++ pages/3_Doctor_Analytics.py
@@ -1,1 +1,29 @@
+import streamlit as st
+import plotly.express as px
+from src.loader import load_data
 
+df = load_data()
+
+st.title("Doctor Analytics")
+
+doctor_perf = (
+    df.groupby("doctor_experience_years")
+    .agg({
+        "patient_satisfaction_score":"mean",
+        "total_bill":"sum"
+    })
+    .reset_index()
+)
+
+fig = px.scatter(
+    doctor_perf,
+    x="doctor_experience_years",
+    y="patient_satisfaction_score",
+    size="total_bill",
+    title="Doctor Experience vs Satisfaction"
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
