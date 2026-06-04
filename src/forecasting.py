3396b2f23feba41e3f550cae9dc4a25bd750d9d3
shraddhaballambettu-student <shraddhaballambettu@gmail.com>
2026-06-04 12:21:38 +0530

    Update forecasting.py

--- src/forecasting.py
+++ src/forecasting.py
@@ -1,1 +1,37 @@
+from prophet import Prophet
+import pandas as pd
 
+
+def revenue_forecast(df):
+
+    revenue = (
+        df.groupby("appointment_month")
+        ["total_bill"]
+        .sum()
+        .reset_index()
+    )
+
+    revenue.columns = [
+        "ds",
+        "y"
+    ]
+
+    revenue["ds"] = pd.to_datetime(
+        revenue["ds"],
+        errors="coerce"
+    )
+
+    model = Prophet()
+
+    model.fit(revenue)
+
+    future = model.make_future_dataframe(
+        periods=12,
+        freq="M"
+    )
+
+    forecast = model.predict(
+        future
+    )
+
+    return model,forecast
