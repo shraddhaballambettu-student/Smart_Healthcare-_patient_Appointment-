5f470c123da71c5b97d0bcabce5759a9819cc545
shraddhaballambettu-student <shraddhaballambettu@gmail.com>
2026-06-04 12:15:09 +0530

    Update loader.py

--- src/loader.py
+++ src/loader.py
@@ -1,1 +1,27 @@
+import pandas as pd
+import streamlit as st
+from config.settings import DATA_PATH
 
+
+@st.cache_data
+def load_data():
+
+    df = pd.read_csv(DATA_PATH)
+
+    return df
+
+
+@st.cache_data
+def get_numeric_columns(df):
+
+    return df.select_dtypes(
+        include=["int64", "float64"]
+    ).columns.tolist()
+
+
+@st.cache_data
+def get_categorical_columns(df):
+
+    return df.select_dtypes(
+        include=["object"]
+    ).columns.tolist()
