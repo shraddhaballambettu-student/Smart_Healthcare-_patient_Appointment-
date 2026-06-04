976a7fbab5b1af38cc75c338be2eb98ebf09c7f8
shraddhaballambettu-student <shraddhaballambettu@gmail.com>
2026-06-04 12:22:10 +0530

    Update anomaly.py

--- src/anomaly.py
+++ src/anomaly.py
@@ -1,1 +1,23 @@
+from sklearn.ensemble import IsolationForest
 
+
+def detect_billing_anomalies(df):
+
+    features = df[
+        [
+            "medicine_cost",
+            "test_cost",
+            "total_bill"
+        ]
+    ]
+
+    model = IsolationForest(
+        contamination=0.02,
+        random_state=42
+    )
+
+    df["anomaly"] = model.fit_predict(
+        features
+    )
+
+    return df
